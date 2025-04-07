4dfd11f180aeb65de0f1734c7f82729f9a68ddb7
reseter1 <nguyenhuutai.reseter@gmail.com>
2025-04-07 00:10:13 +0000

    Some fixes.

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/me.jpg";
+import myImg from "../../Assets/fake_me.jpg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
